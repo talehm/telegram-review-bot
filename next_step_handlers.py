c1355c64b53eb97e8389fdcc95864919f62a7a18
your_username <taleh.muzaffarov@dirs21.team>
2021-11-27 15:40:05 +0100

    -- add photo

--- next_step_handlers.py
+++ next_step_handlers.py
@@ -14,9 +14,9 @@ class NextStepHandlers:
             else:
                 product = db.session.query(Product).filter(Product.id==id)[0]
                 
-                msg = bot.reply_to(message, type(product))
-                msg = bot.send_photo(chat_id, photo='https://www.thewechatagency.com/wp-content/uploads/2017/11/chat_bot-01-660x495.jpg')
-                msg = bot.reply_to(message, product.name)
+                # msg = bot.reply_to(message, type(product))
+                msg = bot.send_photo(chat_id, photo='https://www.thewechatagency.com/wp-content/uploads/2017/11/chat_bot-01-660x495.jpg', caption=product.name)
+                # msg = bot.reply_to(message, product.name)
             #bot.register_next_step_handler(msg, process_sex_step)
         except IndexError as e:
             msg = bot.reply_to(message, "Product ID not found!")
