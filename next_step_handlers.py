ff04e1f548b2d77ac0e54c351ede8fd905ed00ea
your_username <taleh.muzaffarov@dirs21.team>
2021-11-27 21:04:04 +0100

    no message

--- next_step_handlers.py
+++ next_step_handlers.py
@@ -3,7 +3,7 @@ from telebot_creds.credentials import bot
 from app.models import Product
 from app import db
 class NextStepHandlers:
-    def check_order_id(message):
+    def check_order_id(self, message):
         try:
             message_id = message.message_id
             chat_id = message.chat.id
@@ -15,7 +15,7 @@ class NextStepHandlers:
             else:
                 product = db.session.query(Product).filter(Product.id==id)[0]
                 # msg = bot.reply_to(message, type(product))
-                msg = bot.send_photo(chat_id, photo='https://www.thewechatagency.com/wp-content/uploads/2017/11/chat_bot-01-660x495.jpg', caption=product.name, reply_to_message_id=message_id, reply_markup=order_chosen_product_markup())
+                msg = bot.send_photo(chat_id, photo='https://www.thewechatagency.com/wp-content/uploads/2017/11/chat_bot-01-660x495.jpg', caption=product.name, reply_to_message_id=message_id, reply_markup=self.order_chosen_product_markup())
                 # msg = bot.reply_to(message, product.name)
             #bot.register_next_step_handler(msg, process_sex_step)
         except IndexError as e:
@@ -23,7 +23,7 @@ class NextStepHandlers:
         except Exception as e:
             bot.reply_to(message, str(e)) 
 
-    def verify_order_with_agent(message):
+    def verify_order_with_agent(self,message):
         try:
             message_id=message.message_id
             chat_id=message.chat.id
@@ -31,10 +31,10 @@ class NextStepHandlers:
         except:
 
 
-def order_chosen_product_markup():
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 1
+    def order_chosen_product_markup():
+        markup = InlineKeyboardMarkup()
+        markup.row_width = 1
 
-    markup.add(InlineKeyboardButton("\U0001F4EB Order This Product", callback_data='order_chosen_product'),
-            InlineKeyboardButton("\U0001F31F Return Home", callback_data='return_home'),
-    return markup
+        markup.add(InlineKeyboardButton("\U0001F4EB Order This Product", callback_data='order_chosen_product'),
+                InlineKeyboardButton("\U0001F31F Return Home", callback_data='return_home'),
+        return markup 
