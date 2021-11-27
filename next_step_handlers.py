ed7b3bf7fb8927f05e1bd264da47455d62586995
your_username <taleh.muzaffarov@dirs21.team>
2021-11-27 15:26:53 +0100

    fixes

--- next_step_handlers.py
+++ next_step_handlers.py
@@ -15,7 +15,7 @@ class NextStepHandlers:
                 product = db.session.query(Product).filter(Product.id==id)[0]
                 
                 msg = bot.reply_to(message, type(product))
-
+                msg = bot.send_photo(chat_id, photo='https://www.thewechatagency.com/wp-content/uploads/2017/11/chat_bot-01-660x495.jpg')
                 msg = bot.reply_to(message, product.name)
             #bot.register_next_step_handler(msg, process_sex_step)
         except IndexError as e:
